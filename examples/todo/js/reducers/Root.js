c811316426170282b27ca062ba2355411ab49b77
鎌倉 正也 <kamakura_m@waku-2.com>
2015-12-25 18:17:36 +0900

    delete todo

--- examples/todo/js/reducers/Root.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { TodoReducer } from './TodoReducer';
-import { TabReducer } from './TabReducer';
-
-export const RootReducer = combineReducers({
-  tab: TabReducer,
-  todo: TodoReducer
-});
