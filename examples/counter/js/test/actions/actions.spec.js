20c2ed0c860a928a13e50da0b159488d71aace32
mkamakura <kamakura.masaya@gmail.com>
2016-05-11 18:41:11 +0900

    create action tests

---
+++ examples/counter/js/test/actions/actions.spec.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import * as actions from '../../actions/ActionCreator';
+
+test('increment should create increment action', (t) => {
+  t.deepEqual(actions.increment(0), {type: actions.INCREMENT, payload: 0});
+})
+
+test('increment should create decrement action', (t) => {
+  t.deepEqual(actions.decrement(0), {type: actions.DECREMENT, payload: 0});
+})
---
+++ examples/greeting/js/test/actions/actions.spec.js
@@ -0,0 +1,6 @@
+import test from 'ava';
+import * as actions from '../../actions/ActionCreator';
+
+test('updateName should create updateName action', (t) => {
+  t.deepEqual(actions.updateName('Kamakura Masaya'), {type: actions.UPDATE_NAME, payload: 'Kamakura Masaya'});
+})
